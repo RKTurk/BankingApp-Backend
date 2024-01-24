54eb255a556cdebd266d8dba740a79421af230e3
Rizwan Khan Turk <rixwankhan99@gmail.com>
2024-01-24 16:21:35 +0500

    Banking Application Back-end Work Start

--- src/main/java/com/telusko/bankingapplication/BankingappApplication.java
+++ src/main/java/com/telusko/bankingapplication/BankingappApplication.java
@@ -1,13 +1,13 @@
-package com.telusko.quizapp;
+package com.telusko.bankingapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QuizappApplication {
+public class BankingappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QuizappApplication.class, args);
+		SpringApplication.run(BankingappApplication.class, args);
 		//System.out.println("Hello World!");
 	}
 
--- src/main/java/com/telusko/bankingapplication/config/CorsConfig.java
+++ src/main/java/com/telusko/bankingapplication/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.telusko.quizapp.config;
+package com.telusko.bankingapplication.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
